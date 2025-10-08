140da2c40609601af49498915844cb3c5fdee60d
Axi <axidreamdragon@gmail.com>
2025-10-08 12:18:10 +0200

    Change name, implement favicon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fortune Telling
+# Name Fortunes
 
 This is just a simple test for me to get used to PHP. You can put in a name, and it'll say how lucky you are to have this name today!
 
--- public/index.php
+++ public/index.php
@@ -7,8 +7,9 @@ require_once '../src/database.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Lucky Names</title>
-	<link rel="stylesheet" href="index.css">
+	<title>Name Fortunes</title>
+	<link rel="icon" href="/favicon.png">
+	<link rel="stylesheet" href="/index.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
